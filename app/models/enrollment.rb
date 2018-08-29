73e4d3c353d76c82e202d593d69fa22f3aa24eed
Ben Wexler <mixedywex@gmail.com>
2018-08-29 15:10:07 -0400

    going to reset db

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,6 @@
 class Enrollment < ApplicationRecord
+    belongs_to :student
+    belongs_to :cohort
     validates :student_id, presence: true
     validates_uniqueness_of :student_id, :scope => :cohort_id
     validates :cohort_id, presence: true
